a6d295d4a4330b6dea9a15e7432332a651830e32
sudhan <sudhans@gmail.com>
2023-03-03 12:26:48 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const firstName = 'Ajay';
+  const lastName = 'Madhusudhan';
+
+
+ /* const blogObj = 
+  {
+    title : 'My Family',
+    description: 'This is my Family.Meet my family',
+    title1: 'My School',
+    description1: 'This is my school.Visit my school',
+    title2 : 'My Home',
+    description2: 'This is my home. Meet my neighbours'
+  }
+  
+  
+   /*<div>
+      <h1> {getFullName(firstName, lastName)} </h1>
+      </div>
+
+        <div className="blogCard">  
+        <h3> {blogObj.title} </h3>
+        <p> {blogObj.description} </p>
+        </div>
+
+        <div className="blogCard">  
+        <h3> {blogObj.title1} </h3>
+        <p> {blogObj.description1} </p>
+        </div>
+
+        <div className="blogCard">  
+        <h3> {blogObj.title2} </h3>
+        <p> {blogObj.description2} </p>
+        </div>
+        
+        */
+
+  const objArray = [
+    {
+      id: 1,
+      title : 'My Family',
+      description: 'This is my Family.Meet my family'
+    }
+    ,
+    {     
+      id: 2,
+      title: 'My School',
+    description: 'This is my school.Visit my school'}
+    ,
+    {
+      id: 3,
+      title: 'My Home',
+    description: 'This is my home. Meet my neighbours'
+    }
+
+  ]
+
+  const blogCards = objArray.map((item) => 
+  {
+    console.log(item);
+
+   return(
+       
+    <div>
+
+    <div className="blogCard" key = {id}>  
+    <h3> {item.title} </h3>
+    <p> {item.description} </p>
+    </div>
+
+
+    </div>
+
+   );
+      
+    }
+  
+  )
+  
+  return (
+    <div className="App">
+
+      {blogCards}
+        
+        </div>
+
+  );
+
+}
+
+export default App;
